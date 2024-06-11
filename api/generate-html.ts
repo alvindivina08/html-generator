f1e06631d8d6b56d4ccda4fcd30e21603fa1243a
alvindivina08 <alvindivina08@gmail.com>
2024-06-11 13:00:12 -0500

    vercel serverless api setup

--- api/generate-html.ts
+++ api/generate-html.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
 import { renderToStaticMarkup } from "@usewaypoint/email-builder";
-import { Resend } from "resend";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
@@ -9,13 +8,8 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 dotenv.config();
 
 const app = express();
-const resend = new Resend(process.env.RESEND_API_KEY);
 app.use(cors());
 app.use(bodyParser.json());
-
-app.get("/health", (req: Request, res: Response) => {
-  res.status(200).send({ status: "UP" });
-});
 
 const apiKeyMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const apiKey = req.headers["x-api-key"];
@@ -27,34 +21,12 @@ const apiKeyMiddleware = (req: Request, res: Response, next: NextFunction) => {
 };
 
 app.post(
-  "/send-email",
-  apiKeyMiddleware,
-  async (req: Request, res: Response) => {
-    const { document } = req.body;
-    try {
-      const html = renderToStaticMarkup(document, { rootBlockId: "root" });
-      await resend.emails.send({
-        from: "Web Store <hello@alvindivina.com>",
-        to: "alvindivina08@gmail.com",
-        subject: "Thanks for your order! This is your receipt.",
-        html: html,
-      });
-      res.status(200).send({ message: "Email sent successfully!" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ message: "Failed to send email." });
-    }
-  }
-);
-
-app.post(
   "/generate-html",
   apiKeyMiddleware,
   async (req: Request, res: Response) => {
     const { document } = req.body;
     try {
       const html = renderToStaticMarkup(document, { rootBlockId: "root" });
-
       res.status(200).send({ html });
     } catch (error) {
       console.error(error);

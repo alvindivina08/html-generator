a2d450d93ba341245e51af657591215a453af757
alvindivina08 <alvindivina08@gmail.com>
2024-06-11 11:04:23 -0500

    initial commit

---
+++ src/server.js
@@ -0,0 +1,56 @@
+import express from "express";
+import { renderToStaticMarkup } from "@usewaypoint/email-builder";
+import { Resend } from "resend";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const resend = new Resend(process.env.RESEND_API_KEY);
+app.use(cors());
+app.use(bodyParser.json());
+
+const apiKeyMiddleware = (req, res, next) => {
+  const apiKey = req.headers["x-api-key"];
+  if (apiKey && apiKey === process.env.SEND_EMAIL_API_KEY) {
+    next();
+  } else {
+    res.status(403).send({ message: "Forbidden: Invalid API Key" });
+  }
+};
+
+app.post("/send-email", apiKeyMiddleware, async (req, res) => {
+  const { document } = req.body;
+  try {
+    const html = renderToStaticMarkup(document, { rootBlockId: "root" });
+    await resend.emails.send({
+      from: "Web Store <hello@alvindivina.com>",
+      to: "alvindivina08@gmail.com",
+      subject: "Thanks for your order! This is your receipt.",
+      html: html,
+    });
+    res.status(200).send({ message: "Email sent successfully!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Failed to send email." });
+  }
+});
+
+app.post("/generate-html", apiKeyMiddleware, async (req, res) => {
+  const { document } = req.body;
+  try {
+    const html = renderToStaticMarkup(document, { rootBlockId: "root" });
+
+    res.status(200).send({ html });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Failed to generate HTML." });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

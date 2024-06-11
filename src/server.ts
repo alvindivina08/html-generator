f58cbfcf03520e10ebaa2f620f3f2a3ffe9b9406
alvindivina08 <alvindivina08@gmail.com>
2024-06-11 11:41:10 -0500

    change port

--- src/server.ts
+++ src/server.ts
@@ -58,7 +58,7 @@ app.post(
   }
 );
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
